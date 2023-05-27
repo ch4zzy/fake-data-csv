e616f589e15e22f338ff7d020492fe93612891bb
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-27 17:39:47 +0300

    formatted: by pre-commit

--- apps/fakecsv/apps.py
+++ apps/fakecsv/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FakecsvConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.fakecsv'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.fakecsv"
