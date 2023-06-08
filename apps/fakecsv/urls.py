307c200c60834aa551448b593980ada72252789e
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-08 21:34:10 +0300

    edited: added path for detail and download

--- apps/fakecsv/urls.py
+++ apps/fakecsv/urls.py
@@ -1,6 +1,14 @@
 from django.urls import include, path
 
-from apps.fakecsv.views import create_schema, delete_schema, edit_schema, schema_list, delete_column
+from apps.fakecsv.views import (
+    create_schema,
+    delete_column,
+    delete_schema,
+    detail_schema,
+    download_data_set,
+    edit_schema,
+    schema_list,
+)
 
 app_name = "fakecsv"
 
@@ -10,5 +18,7 @@ urlpatterns = [
     path("edit-schema/<int:pk>/", edit_schema, name="edit_schema"),
     path("delete-schema/<int:pk>/", delete_schema, name="delete_schema"),
     path("delete-column/<int:pk>/", delete_column, name="delete_column"),
+    path("detail-schema/<int:pk>/", detail_schema, name="detail_schema"),
+    path("download-data-set/<int:data_set_id>/", download_data_set, name="download_data_set"),
     path("list/", schema_list, name="schema_list"),
 ]
