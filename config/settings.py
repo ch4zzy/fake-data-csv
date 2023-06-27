527f88bf74309e64dfe52e6f3648194607d9929d
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-27 17:50:59 +0300

    edited: aws config with error

--- config/settings.py
+++ config/settings.py
@@ -110,15 +110,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 LOGIN_REDIRECT_URL = "/list/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+# Crispy forms
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+# AWS S3 config
+
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_NAME = env("AWS_S3_SIGNATURE_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = env.bool("AWS_S3_FILE_OVERWRITE")
+AWS_DEFAULT_ACL = env("AWS_DEFAULT_ACL")
+AWS_S3_VERITY = env.bool("AWS_S3_VERITY")
+DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
