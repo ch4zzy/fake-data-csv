53a607d53763237df4349affae8fa645d1f40404
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-06-29 19:11:07 +0300

    edited: main changes for aws

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ LOGIN_REDIRECT_URL = "/list/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
 # Crispy forms
@@ -134,6 +134,20 @@ AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_SIGNATURE_NAME = env("AWS_S3_SIGNATURE_NAME")
 AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = env.bool("AWS_S3_FILE_OVERWRITE")
-AWS_DEFAULT_ACL = env("AWS_DEFAULT_ACL")
-AWS_S3_VERITY = env.bool("AWS_S3_VERITY")
-DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
+
+
+# static settings
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
+AWS_LOCATION = "static"
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+# media settings
+
+MEDIA_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, "media")
+DEFAULT_FILE_STORAGE = "config.storage_backends.MediaStorage"
