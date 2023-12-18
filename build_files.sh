55d21ef317f44fa310272cd176f91cab034c327d
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-12-18 16:49:18 +0200

    fix 7

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-
-python3.9 manage.py migrate
-
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@a.com', 'admin')" | python3.9 manage.py shell
-
