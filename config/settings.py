a2e6aec64fb44d3a8d4b32224c5a5817385630ab
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-01-06 18:57:38 +0200

    signals

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.fakecsv",
+    "apps.fakecsv.apps.FakecsvConfig",
     "bootstrap5",
     "crispy_forms",
     "crispy_bootstrap5",
